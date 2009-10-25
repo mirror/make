5f188b39a49a7eb0d06979b451863dd7ff26a074
Paul Smith <psmith@gnu.org>
2009-10-25 18:56:44 +0000

    Update copyright years.

--- tests/test_driver.pl
+++ tests/test_driver.pl
@@ -6,7 +6,8 @@
 # Modified 92-02-11 through 92-02-22 by Chris Arthur to further generalize.
 #
 # Copyright (C) 1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999, 2000,
-# 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008 Free Software Foundation, Inc.
+# 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009 Free Software
+# Foundation, Inc.
 # This file is part of GNU Make.
 #
 # GNU Make is free software; you can redistribute it and/or modify it under
