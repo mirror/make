9a9f83e8b52a3923efc6b4cf8c8f444773a30953
Paul Smith <psmith@gnu.org>
2011-09-02 05:37:54 +0000

    Ensure that -n takes precedence over -t. Patch from Michael Witten <mfwitten@gmail.com>

--- remake.c
+++ remake.c
@@ -1113,6 +1113,10 @@ touch_file (struct file *file)
   if (!silent_flag)
     message (0, "touch %s", file->name);
 
+  /* Print-only (-n) takes precedence over touch (-t).  */
+  if (just_print_flag)
+    return 0;
+
 #ifndef	NO_ARCHIVES
   if (ar_name (file->name))
     return ar_touch (file->name);
