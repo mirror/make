715a11735f910d69b141c3b851ca1a806907f43f
Eli Zaretskii <eliz@gnu.org>
2012-01-25 18:16:14 +0000

    function.c (define_new_function): Fix format strings in calls to `fatal'.

--- function.c
+++ function.c
@@ -2424,10 +2424,10 @@ define_new_function(const struct floc *flocp,
   if (len > 255)
     fatal (flocp, _("Function name too long: %s\n"), name);
   if (min < 0 || min > 255)
-    fatal (flocp, _("Invalid minimum argument count (%d) for function %s%s\n"),
+    fatal (flocp, _("Invalid minimum argument count (%d) for function %s\n"),
            min, name);
   if (max < 0 || max > 255 || max < min)
-    fatal (flocp, _("Invalid maximum argument count (%d) for function %s%s\n"),
+    fatal (flocp, _("Invalid maximum argument count (%d) for function %s\n"),
            max, name);
 
   ent->name = name;
