a87ff20158eead839d9457e240934bdc05ba509d
Eli Zaretskii <eliz@gnu.org>
2013-05-01 18:15:16 +0300

    job.c (start_job_command) [WINDOWS32]: Make the same fix for  MS-Windows as the previous commit did for Posix platforms.

--- job.c
+++ job.c
@@ -1857,8 +1857,10 @@ start_job_command (struct child *child)
       sync_Path_environment();
 
 #ifdef OUTPUT_SYNC
-          /* Divert child output into tempfile(s) if output_sync in use. */
-          if (output_sync)
+          /* Divert child output if output_sync in use.  Don't capture
+             recursive make output unless we are synchronizing "make" mode.  */
+          if (output_sync && (output_sync == OUTPUT_SYNC_MAKE
+                              || !(flags & COMMANDS_RECURSE)))
 	    hPID = process_easy(argv, child->environment,
 				child->outfd, child->errfd);
 	  else
