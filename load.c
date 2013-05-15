c21c1455fdfc6e87d75941f48841c72903e1e0f4
Paul Smith <psmith@gnu.org>
2013-05-14 22:53:42 -0400

    Add requirement for plugin_is_GPL_compatible symbol in loaded objects.

--- load.c
+++ load.c
@@ -71,6 +71,12 @@ load_object (const gmk_floc *flocp, int noerror,
         return NULL;
       }
 
+    /* Assert that the GPL license symbol is defined.  */
+    symp = dlsym (*dlp, "plugin_is_GPL_compatible");
+    if (! symp)
+      fatal (flocp, _("Loaded object %s is not declared to be GPL compatible"),
+             ldname);
+
     symp = dlsym (*dlp, symname);
     if (! symp)
       fatal (flocp, _("Failed to load symbol %s from %s: %s"),
