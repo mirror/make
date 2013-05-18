943c06de479797fcda377c1cb95c644aace14919
Eli Zaretskii <eliz@gnu.org>
2013-05-18 14:03:26 +0300

    Fix MS-Windows build with Guile.
     guile.c: Move inclusion of makeint.h before gnumake.h.  This
     order must be observed when building Make, because gnumake.h must
     be included with GMK_BUILDING_MAKE defined, which makeint.h
     already does.  Otherwise, the linker will look for, and fail to
     find, gmk_* functions in some external dynamic library.

--- guile.c
+++ guile.c
@@ -14,9 +14,9 @@ A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
 You should have received a copy of the GNU General Public License along with
 this program.  If not, see <http://www.gnu.org/licenses/>.  */
 
+#include "makeint.h"
 #include "gnumake.h"
 
-#include "makeint.h"
 #include "debug.h"
 #include "dep.h"
 #include "variable.h"
