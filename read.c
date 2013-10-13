70df3c35bf2f0272f5346a57410a31d9d7d0735c
Paul Smith <psmith@gnu.org>
2013-10-13 16:01:22 -0400

    [SV 40139] Modify "missing separator" for better translation

--- read.c
+++ read.c
@@ -1120,9 +1120,10 @@ eval (struct ebuffer *ebuf, int set_default)
             if (*p2 != '\0')
               /* There's no need to be ivory-tower about this: check for
                  one of the most common bugs found in makefiles...  */
-              fatal (fstart, _("missing separator%s"),
-                     (cmd_prefix == '\t' && !strneq (line, "        ", 8))
-                     ? "" : _(" (did you mean TAB instead of 8 spaces?)"));
+              if (cmd_prefix == '\t' && !strneq (line, "        ", 8))
+                fatal (fstart, _("missing separator (did you mean TAB instead of 8 spaces?)"));
+              else
+                fatal (fstart, _("missing separator"));
             continue;
           }
 
