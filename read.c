d654bff45764a134d74aaf57871754b33d016ff0
Paul Smith <psmith@gnu.org>
2013-10-19 12:07:31 -0400

    * read.c (eval): Avoid GCC warning to add braces.

--- read.c
+++ read.c
@@ -1117,14 +1117,15 @@ eval (struct ebuffer *ebuf, int set_default)
            it.  If so, we can't parse this line so punt.  */
         if (wtype == w_eol)
           {
-            if (*p2 != '\0')
-              /* There's no need to be ivory-tower about this: check for
-                 one of the most common bugs found in makefiles...  */
-              if (cmd_prefix == '\t' && !strneq (line, "        ", 8))
-                fatal (fstart, _("missing separator (did you mean TAB instead of 8 spaces?)"));
-              else
-                fatal (fstart, _("missing separator"));
-            continue;
+            if (*p2 == '\0')
+              continue;
+
+            /* There's no need to be ivory-tower about this: check for
+               one of the most common bugs found in makefiles...  */
+            if (cmd_prefix == '\t' && !strneq (line, "        ", 8))
+              fatal (fstart, _("missing separator (did you mean TAB instead of 8 spaces?)"));
+            else
+              fatal (fstart, _("missing separator"));
           }
 
         /* Make the colon the end-of-string so we know where to stop
