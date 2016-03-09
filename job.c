14b2d7effb0afd75dfd1ed2534e331784f7d2977
Paul Smith <psmith@gnu.org>
2016-03-09 00:18:57 -0500

    * job.c (exec_command): [SV 47365] Show error on exec failure.

--- job.c
+++ job.c
@@ -2322,7 +2322,13 @@ exec_command (char **argv, char **envp)
   switch (errno)
     {
     case ENOENT:
-      OS (error, NILF, _("%s: Command not found"), argv[0]);
+      /* We are in the child: don't use the output buffer.
+         It's not right to run fprintf() here!  */
+      if (makelevel == 0)
+        fprintf (stderr, _("%s: %s: Command not found\n"), program, argv[0]);
+      else
+        fprintf (stderr, _("%s[%u]: %s: Command not found\n"),
+                 program, makelevel, argv[0]);
       break;
     case ENOEXEC:
       {
