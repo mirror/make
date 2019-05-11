4a31ca461ffa845ba85f5190b767c50285c00141
Paul Smith <psmith@gnu.org>
2019-05-11 10:30:26 -0400

    Add developer customizations
    * .ccls: Configure the ccls LSP server
    * .dir-locals.el: Reset some parameters for ccls and lsp-mode
    * .gitignore: Update for GTAGS and ccls
    * maintMakefile: Don't search hidden directories for sources

---
+++ .dir-locals.el
@@ -0,0 +1,17 @@
+(
+ (nil . ((bug-reference-bug-regexp . "\\(\\)\\bSV[- ]\\([0-9]+\\)")
+         (bug-reference-url-format . "https://savannah.gnu.org/bugs/?%s")
+         (ccls-initialization-options . (:index (:threads 6
+                                                 :initialBlacklist ("/make-[0-9]" "tests/work/" "/\\.deps" "/\\..*cache" "/\\.git"))))
+         (lsp-file-watch-ignored . ("/\\.git$"
+                                    "/\\..*cache$"
+                                    ;; autotools content
+                                    "/\\.deps$"
+                                    "/autom4te\\.cache$"
+                                    "/build-aux$"
+                                    ;; make-specific content
+                                    "/doc/manual$"
+                                    "/tests/work$"
+                                    "/make-[0-9]"))
+         ))
+)
