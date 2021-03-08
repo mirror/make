7b0a7418579a57934d4871205ffa29d0865dcc0e
Paul Smith <psmith@gnu.org>
2021-03-07 19:04:48 -0500

    * .dir-locals.el: Update for newer LSP config

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,18 +1,21 @@
 (
  (nil . ((bug-reference-bug-regexp . "\\(\\)\\bSV[- ]\\([0-9]+\\)")
          (bug-reference-url-format . "https://savannah.gnu.org/bugs/?%s")
-         (ccls-initialization-options . (:index (:threads 6
-                                                 :initialBlacklist ("/make-[0-9]" "tests/work/" "/\\.deps" "/\\..*cache" "/\\.git"))))
-         (lsp-file-watch-ignored . ("/\\.git$"
-                                    "/\\..*cache$"
-                                    ;; autotools content
-                                    "/\\.deps$"
-                                    "/autom4te\\.cache$"
-                                    "/build-aux$"
-                                    ;; make-specific content
-                                    "/doc/manual$"
-                                    "/tests/work$"
-                                    "/make-[0-9]"))
+         (ccls-initialization-options
+          . (:index (:threads 6
+                     :initialBlacklist ["/make-[0-9]" "tests/work/" "/\\.deps"
+                                        "/\\..*cache" "/\\.git"])))
+         (lsp-file-watch-ignored-directories
+          . ("[/\\\\]\\.git$"
+             "[/\\\\]\\..*cache$"
+             ;; autotools content
+             "[/\\\\]\\.deps$"
+             "[/\\\\]autom4te\\.cache$"
+             "[/\\\\]build-aux$"
+             ;; make-specific content
+             "[/\\\\]doc[/\\\\]manual$"
+             "[/\\\\]tests[/\\\\]work$"
+             "[/\\\\]make-[0-9]"))
          ))
  (c-mode . ((c-file-style . "gnu")))
 )
